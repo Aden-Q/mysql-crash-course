582e5b4a3ec3a4ea7ef8163dacd3e0b031d134f5
aden-q <627374286@qq.com>
2022-04-24 19:20:13 -0500

    Update

--- src/03-working-with-mysql.sql
+++ src/03-working-with-mysql.sql
@@ -4,3 +4,14 @@
 SHOW DATABASES;
 -- change database
 USE mydb;
+
+-- show tables
+SHOW TABLES;
+
+-- show scheme
+SHOW COLUMNS FROM customers;
+DESCRIBE customers;
+
+-- some other show info commands
+SHOW STATUS;
+HELP SHOW;
