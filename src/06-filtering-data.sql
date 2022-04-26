6dbe004f0cb4b534b67f789f534d9cf96cd00dbd
aden-q <627374286@qq.com>
2022-04-26 09:20:32 -0500

    Update chapter 6

--- src/06-filtering-data.sql
+++ src/06-filtering-data.sql
@@ -1,1 +1,11 @@
 -- Chapter 6 Filtering Data
+
+-- filter
+SELECT prod_name, prod_price FROM products WHERE prod_price = 2.50;
+SELECT prod_name, prod_price FROM products WHERE prod_price < 10;
+SELECT prod_name, prod_price FROM products WHERE prod_price <> 2.50;
+SELECT prod_name, prod_price FROM products WHERE prod_price BETWEEN 5 and 10;
+SELECT prod_name, prod_price FROM products WHERE prod_price IS NULL;
+
+
+
