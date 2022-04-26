7c21b8120ccada4e66f0f9b93f6ea36cf75c107e
aden-q <627374286@qq.com>
2022-04-26 10:00:27 -0500

    Update chapter 9

--- src/09-searching-using-regular-expressions.sql
+++ src/09-searching-using-regular-expressions.sql
@@ -1,1 +1,27 @@
 -- Chapter 9 Searching Using Regular Expressions
+
+SELECT * FROM products WHERE prod_name REGEXP '1000';
+SELECT * FROM products WHERE prod_name REGEXP '.000';
+
+-- OR clause
+SELECT * FROM products WHERE prod_name REGEXP '1000|2000';
+
+-- search one of characters in a list
+SELECT * FROM products WHERE prod_name REGEXP '[123] ton';
+SELECT * FROM products WHERE prod_name REGEXP '[^123] ton';
+
+-- range search
+SELECT * FROM products WHERE prod_name REGEXP '[0-9] ton';
+
+-- search special characters
+SELECT * FROM products WHERE prod_name REGEXP '\\.';
+
+-- predefine character set
+SELECT * FROM products WHERE prod_name REGEXP '[:alnum:] ton';
+
+-- search multiple instances
+SELECT * FROM products WHERE prod_name REGEXP '\\([0-9] sticks?\\)';
+SELECT * FROM products WHERE prod_name REGEXP '[:digit:]{4}';
+
+-- search position
+SELECT * FROM products WHERE prod_name REGEXP '^[0-9\\.]';
