9003acf1de6a24d3bb0cbbaf24b7c88e954a962b
aden-q <627374286@qq.com>
2022-04-26 12:29:11 -0500

    Add chapter 13

--- src/13-grouping-data.sql
+++ src/13-grouping-data.sql
@@ -1,3 +1,26 @@
 -- Chapter 13 Grouping Data
 
 -- group by
+DESCRIBE products;
+SELECT * FROM products;
+SELECT vend_id, COUNT(*) AS num_prods
+FROM products
+GROUP BY vend_id;
+
+-- filter group
+SELECT vend_id, COUNT(*) AS num_prods
+FROM products
+GROUP BY vend_id
+HAVING num_prods >= 3;
+
+SELECT vend_id, COUNT(*) AS num_prods
+FROM products
+WHERE prod_price >= 10
+GROUP BY vend_id
+HAVING num_prods >= 3;
+
+SELECT order_num, SUM(quantity * item_price) AS order_price
+FROM orderitems
+GROUP BY order_num
+HAVING order_price >= 50
+ORDER BY order_price;
