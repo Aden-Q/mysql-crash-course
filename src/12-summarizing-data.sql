d3796e4f088025b27786009a71d694cc0330b905
aden-q <627374286@qq.com>
2022-04-26 12:12:13 -0500

    Update chapter 12

--- src/12-summarizing-data.sql
+++ src/12-summarizing-data.sql
@@ -1,1 +1,27 @@
 -- Chapter 12 Summarizing Data
+
+-- aggregate functions
+DESCRIBE products;
+
+-- average
+SELECT AVG(prod_price) AS avg_price
+FROM products;
+
+-- count
+SELECT COUNT(*) AS num_cust
+FROM customers;
+
+-- max
+SELECT MAX(prod_price) AS max_price
+FROM products;
+
+-- sum
+SELECT SUM(prod_price) AS sum_price
+FROM products;
+
+-- composite
+SELECT COUNT(*) AS num_items,
+    MIN(prod_price) AS price_min,
+    MAX(prod_price) AS price_max,
+    AVG(prod_price) AS price_avg
+FROM products;
