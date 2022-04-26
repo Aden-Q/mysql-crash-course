b4d1c35616f818b22c4a01bd22f795df891647ae
aden-q <627374286@qq.com>
2022-04-26 12:02:44 -0500

    Update chapter 11

--- src/11-using-data-manipulation-functions.sql
+++ src/11-using-data-manipulation-functions.sql
@@ -1,1 +1,14 @@
 -- Chapter 11 Using Data Manipulation Functions
+
+-- string function
+SELECT vend_name, Upper(vend_name) AS vend_name_upcase
+FROM vendors
+ORDER BY vend_name;
+
+-- date
+SELECT cust_id, order_num
+FROM orders
+WHERE Date(order_date) = '2005-09-01';
+SELECT cust_id, order_num
+FROM orders
+WHERE Year(order_date) = 2005 AND Month(order_date) = 9;
