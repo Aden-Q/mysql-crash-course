24c5d50b3d2356036f24b7fc07819352bb323551
aden-q <627374286@qq.com>
2022-04-26 09:29:40 -0500

    Update chapter 7

--- src/07-advanced-data-filtering.sql
+++ src/07-advanced-data-filtering.sql
@@ -1,1 +1,9 @@
 -- Chapter 7 Advanced Data Filtering
+
+-- filter
+SELECT * FROM products WHERE vend_id = 1003 AND prod_price <= 10;
+SELECT * FROM products WHERE vend_id = 1003 OR vend_id = 1002;
+
+-- range query
+SELECT * FROM products WHERE vend_id IN (1002, 1003) ORDER BY prod_price;
+SELECT * FROM products WHERE vend_id NOT IN (1002, 1003) ORDER BY prod_price;
