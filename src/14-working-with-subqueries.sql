a94de05c904d8ffb264ae0c90bc05255938f4399
aden-q <627374286@qq.com>
2022-04-26 12:41:06 -0500

    Update

--- src/14-working-with-subqueries.sql
+++ src/14-working-with-subqueries.sql
@@ -1,1 +1,19 @@
 -- Chapter 14 Working with Subqueries
+
+-- subquery
+
+SELECT cust_name, cust_contact
+FROM customers
+WHERE cust_id IN
+    (SELECT cust_id
+    FROM orders
+    WHERE order_num IN
+        (SELECT order_num
+        FROM orderitems
+        WHERE prod_id = 'TNT2'));
+
+SELECT cust_name, cust_state,
+    (SELECT COUNT(*)
+    FROM orders
+    WHERE orders.cust_id = customers.cust_id) AS orders
+FROM customers;
