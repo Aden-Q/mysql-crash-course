9abc2c1594de040a71bf2a486bc1121c8d6e3a0f
aden-q <627374286@qq.com>
2022-04-26 09:39:20 -0500

    Update chapter 8

--- src/08-using-wildcard-filtering.sql
+++ src/08-using-wildcard-filtering.sql
@@ -1,1 +1,9 @@
 -- Chapter 8 Using Wildcard Filtering
+
+-- search pattern
+-- any number characters
+SELECT * FROM products WHERE prod_name LIKE 'Jet%';
+SELECT * FROM products WHERE prod_name LIKE '%anvil%';
+
+-- one arbitrary character
+SELECT * FROM products WHERE prod_name LIKE '_ ton anvil';
