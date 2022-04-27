13794da636e68ed8de7c0d3cc3553e2239d7c05b
aden-q <627374286@qq.com>
2022-04-26 22:45:21 -0500

    Add chapter 20

--- src/20-updating-and-deleting-data.sql
+++ src/20-updating-and-deleting-data.sql
@@ -1,1 +1,17 @@
 -- Chapter 20 Updating and Deleting Data
+
+-- update
+-- update a single column
+UPDATE customers
+SET cust_email = 'elmer@fudd.com'
+WHERE cust_id = 10005;
+
+-- update multiple column
+UPDATE customers
+SET cust_email = 'elmer@fudd.com',
+    cust_name = 'The Fudds'
+WHERE cust_id = 10005;
+
+-- delete
+DELETE FROM customers
+WHERE cust_id = 10006;
