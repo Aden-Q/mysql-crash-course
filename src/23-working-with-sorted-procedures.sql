ade1d3efcaa63c331f4c99acbc237417d4aeceb5
aden-q <627374286@qq.com>
2022-04-27 09:27:35 -0500

    Add chapter 23

--- src/23-working-with-sorted-procedures.sql
+++ src/23-working-with-sorted-procedures.sql
@@ -1,1 +1,42 @@
 -- Chapter 23 Working with Sorted Procedures
+
+-- create procedure
+CREATE PROCEDURE productpricing()
+BEGIN
+    SELECT Avg(prod_price) AS priceaverage
+    FROM products;
+END;
+
+-- invoke procedure
+CALL productpricing();
+
+-- delete procedure
+DROP PROCEDURE IF EXISTS productpricing;
+
+CREATE PROCEDURE productpricing(
+    OUT pl DECIMAL(8, 2),
+    OUT ph DECIMAL(8, 2),
+    OUT pa DECIMAL(8, 2)
+)
+BEGIN
+    SELECT Min(prod_price)
+    INTO pl
+    FROM products;
+    SELECT Max(prod_price)
+    INTO ph
+    FROM products;
+    SELECT Avg(prod_price)
+    INTO pa
+    FROM products;
+END;
+
+CALL productpricing(
+    @pricelow,
+    @pricehigh,
+    @priceaverage);
+
+SELECT @pricelow;
+SELECT @pricehigh;
+SELECT @priceaverage;
+
+SHOW CREATE PROCEDURE productpricing;
