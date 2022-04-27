2f16e69c7ef45dc45a76f126ae68d764b54dc491
aden-q <627374286@qq.com>
2022-04-26 21:28:16 -0500

    Add chapter 17

--- src/17-combining-queries.sql
+++ src/17-combining-queries.sql
@@ -1,1 +1,19 @@
 -- Chapter 17 Combining Queries
+
+-- union
+SELECT vend_id, prod_id, prod_price
+FROM products
+WHERE prod_price <= 5
+UNION
+SELECT vend_id, prod_id, prod_price
+FROM products
+WHERE vend_id REGEXP '1001|1002';
+
+-- return all rows
+SELECT vend_id, prod_id, prod_price
+FROM products
+WHERE prod_price <= 5
+UNION ALL
+SELECT vend_id, prod_id, prod_price
+FROM products
+WHERE vend_id REGEXP '1001|1002';
