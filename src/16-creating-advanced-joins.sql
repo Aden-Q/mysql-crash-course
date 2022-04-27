f464322b3d4129f71b59121b6c8f80511605530f
aden-q <627374286@qq.com>
2022-04-26 21:16:28 -0500

    Add chapter 16

--- src/16-creating-advanced-joins.sql
+++ src/16-creating-advanced-joins.sql
@@ -1,1 +1,24 @@
 -- Chapter 16 Creating Advanced Joins
+
+-- advanced joins
+SELECT prod_id, prod_name
+FROM products
+WHERE vend_id = 
+    (SELECT vend_id
+    FROM products
+    WHERE prod_id = 'DTNTR');
+
+SELECT p1.prod_id, p2.prod_name
+FROM products AS p1, products AS p2
+WHERE p1.vend_id = p2.vend_id
+    AND p2.prod_id = 'DTNTR';
+
+-- outer join
+SELECT customers.cust_id, orders.order_num
+FROM customers LEFT OUTER JOIN orders
+    ON customers.cust_id = orders.cust_id;
+
+SELECT customers.cust_name, customers.cust_id, COUNT(orders.order_num) AS num_orders
+FROM customers LEFT OUTER JOIN orders
+ON customers.cust_id = orders.cust_id
+GROUP BY customers.cust_id;
