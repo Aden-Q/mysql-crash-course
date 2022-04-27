ad1b418009fc30877f2fe05357e93539c7709035
aden-q <627374286@qq.com>
2022-04-26 22:10:13 -0500

    Add chapter 18

--- src/18-full-text-searching.sql
+++ src/18-full-text-searching.sql
@@ -1,1 +1,15 @@
 -- Chapter 18 Full Text Searching
+
+-- full text searching
+SELECT note_text
+FROM productnotes
+WHERE Match(note_text) Against('rabbit');
+
+SELECT note_text
+FROM productnotes
+WHERE note_text LIKE '%rabbit%';
+
+-- query expansion
+SELECT note_text
+FROM productnotes
+WHERE Match(note_text) Against('rabbit' WITH QUERY EXPANSION);
