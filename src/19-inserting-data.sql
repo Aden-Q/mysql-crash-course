ec5450daf3da156b7c853f160121974382118791
aden-q <627374286@qq.com>
2022-04-26 22:35:31 -0500

    Add chapter 19

--- src/19-inserting-data.sql
+++ src/19-inserting-data.sql
@@ -1,1 +1,35 @@
 -- Chapter 19 Inserting Data
+
+-- insertion
+DESCRIBE customers;
+
+INSERT INTO customers
+VALUES (NULL,
+    'Pep E. LaPew',
+    '100 Main Street',
+    'Los Angeles',
+    'CA',
+    '90046',
+    'USA',
+    NULL,
+    NULL);
+
+INSERT INTO customers(
+    cust_id,
+    cust_email,
+    cust_name,
+    cust_address,
+    cust_city,
+    cust_state,
+    cust_zip,
+    cust_country
+)
+SELECT cust_id,
+    cust_email,
+    cust_name,
+    cust_address,
+    cust_city,
+    cust_state,
+    cust_zip,
+    cust_country
+FROM custnew;
